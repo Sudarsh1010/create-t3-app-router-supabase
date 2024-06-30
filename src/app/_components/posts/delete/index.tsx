dbbd1d3150600ea4d057d5259b08428d61d34c82
Sudarsh1010 <sudarshjain1010@gmail.com>
2024-06-30 23:14:35 +0530

    dumb typo

--- src/app/_components/posts/delete/index.tsx
+++ src/app/_components/posts/delete/index.tsx
@@ -65,8 +65,8 @@ export const DeletePost = ({
         <AlertDialogHeader>
           <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
           <AlertDialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
+            This action cannot be undone. This will permanently delete your post
+            from our servers.
           </AlertDialogDescription>
         </AlertDialogHeader>
 
