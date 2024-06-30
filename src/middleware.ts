030b6631a88f0187f00551f077958ce7bae3835b
Sudarsh1010 <sudarshjain1010@gmail.com>
2024-06-30 23:43:28 +0530

    remove trpc from middleware invocation

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,11 +10,12 @@ export const config = {
   matcher: [
     /*
      * Match all request paths except for the ones starting with:
+     * - api/trpc (handled by privateProcedure)
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      * Feel free to modify this pattern to include more paths.
      */
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!api/trpc|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
